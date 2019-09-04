674f3d7677d6fb5a7382539a2eae804e27d8adf8
Ivan Meireles Da Silva <isilva@meuacesso.com.br>
2019-09-04 12:14:28 -0300

    ajustes e testes

---
+++ PoC.ES.Tests/Infra/Repositories/Limits/CompanyRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using PoC.ES.Api.Domain.Entities.Limits;
+using PoC.ES.Api.Domain.Repositories.Limits;
+using PoC.ES.Api.Domain.Types.Limits;
+using PoC.ES.Api.Infra;
+using PoC.ES.Api.Infra.Repositories.Limits;
+using System;
+using Xunit;
+
+namespace PoC.ES.Tests.Infra.Repositories.Limits
+{
+    public class CompanyRepositoryTest
+    {
+        private ICompanyRepository _repository;
+
+        public CompanyRepositoryTest()
+        {
+            var url = "http://localhost:8080";
+            _repository = new CompanyRepository(url);
+            Settings.LoadDatabase(url);
+        }
+
+        [Fact]
+        public void AddNewCustomerSimple()
+        {
+            //arrange
+            var random = new Random();
+            var company = Company.Create($"ACESSO{random.Next(1000, 10000)}");
+            company.AddLimit(Limit.Create(LimitType.CashIn, "TED"));
+
+            //act
+            _repository.AddOrUpdate(company);
+            var companyFound = _repository.Get(company.Id);
+
+            //assert
+            Assert.NotNull(companyFound);
+            Assert.Equal(company.Id, companyFound.Id);
+            Assert.True(companyFound.Limits.Count > 0);
+        }
+    }
+}
