04eb3a4d1e8b77027fb4205106560a84fa08e300
Ivan Meireles Da Silva <isilva@meuacesso.com.br>
2019-09-11 21:04:27 -0300

    ajustes

--- PoC.ES.Console/Model/FeedEvent.cs
+++ PoC.ES.Console/Model/FeedEvent.cs
@@ -9,7 +9,7 @@ namespace PoC.ES.ConsoleApp.Model
         [JsonIgnore]
         public string Id { get; set; }
         public string AggregatedId { get; set; }
-        [JsonConverter(typeof(StringEnumConverter))] public Category Category { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))] public Category? Category { get; set; }
         [JsonConverter(typeof(StringEnumConverter))] public Status? Status { get; set; }
         public string Name { get; set; }
         public string TAG { get; set; }
